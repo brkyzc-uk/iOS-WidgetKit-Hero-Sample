6393afb80fcf27d4789fd07dc4d3da594c5e112d
brkyzc-uk <burak.yazici@anadolu.edu.tr>
2022-04-25 13:38:01 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Burak YAZICI on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
